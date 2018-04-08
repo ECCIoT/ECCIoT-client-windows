03252b10c3165154ddd43293003b12c0e58e02bc
Guijie Lee <landriesnidis@yeah.net>
2018-04-09 00:52:06 +0800

    SDK测试窗体

---
+++ Form/FormSDK.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ECC_client_windows
+{
+    partial class FormSDK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(23, 72);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 30);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseSelectable = true;
+            this.btnConnect.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // FormSDK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 525);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "FormSDK";
+            this.Text = "FormSDK";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton btnConnect;
+    }
+}
---
+++ Form/FormSDK.cs
@@ -0,0 +1,64 @@
+﻿using ECC_sdk_windows.Manager;
+using ECC_sdk_windows.Manager.Args;
+using ECC_sdk_windows.Manager.Function;
+using ECCIoT_sdk_windows;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ECC_client_windows
+{
+    public partial class FormSDK : MetroForm,IEccEvevt
+    {
+        
+        public FormSDK()
+        {
+            InitializeComponent();
+            
+        }
+
+        void IEccEvevt.EccEvent_Alarm(AlarmEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEccEvevt.EccEvent_APIKeyInvalid(APIKeyInvalidEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEccEvevt.EccEvent_APIKeyVerified(APIKeyVerifiedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEccEvevt.EccEvent_CheckAPIKey(CheckAPIKeyEventArgs args)
+        {
+            ECCIoT.Manager.EccCmd_CheckAPIKey(new SendAPIKeyCmdArgs { APIKey = "abcdefg12345678" },successful=> { },failure=> { });
+        }
+
+        void IEccEvevt.EccEvent_UpdateItemsData(UpdateItemsDataEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            ECCIoT.Connect(new EccManager(this), 
+                successful => {
+                    MessageBox.Show("连接成功！");
+                }, 
+                failure => {
+                    MessageBox.Show("连接失败！");
+                });
+        }
+    }
+}
